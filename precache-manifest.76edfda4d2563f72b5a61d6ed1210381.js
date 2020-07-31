53f6495fbb61fd651ee23dc09a2817173ad45429
Mufti Hyder Ali <36014695+mhaf4krr@users.noreply.github.com>
2020-07-31 12:00:35 +0530

    Add files via upload

---
+++ precache-manifest.76edfda4d2563f72b5a61d6ed1210381.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dec336a93876a270479b7da7c4deb59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fba73d3dd4d929c0da3f",
+    "url": "/static/js/2.cfe4b2ff.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cfe4b2ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d5ab2de7fd072ad2ea",
+    "url": "/static/js/main.43d75a56.chunk.js"
+  },
+  {
+    "revision": "038692e598fee278b683",
+    "url": "/static/js/runtime-main.daff27af.js"
+  }
+]);
